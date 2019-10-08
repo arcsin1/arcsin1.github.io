40474998da978af5e8e23b916a7cfb3f6dd67625
arcsin1 <yun.zy@dtwave-inc.com>
2019-10-08 19:17:51 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,50 +15,63 @@ self.addEventListener('install',function(event){
     })
   )
 })
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
 // 
 self.addEventListener('activate', event => {
     console.log('V1 now ready to handle fetches!');
     // 这里是更新
   });
 
-  // 判断是否跨域了
-const isCORSRequest = function(url, host) {
-    return url.search(host) === -1;
-};
-const isValidResponse = function(response) {
-	return response && response.status >= 200 && response.status < 400 && response.type !== 'basic'
-};
+//   // 判断是否跨域了
+// const isCORSRequest = function(url, host) {
+//     return url.search(host) === -1;
+// };
+// const isValidResponse = function(response) {
+// 	return response && response.status >= 200 && response.status < 400 && response.type !== 'basic'
+// };
 
-const HOST_NAME = location.host;
-console.log(location.host)
+// const HOST_NAME = location.host;
+// console.log(location.host)
 
-const handleFetchRequest = function(req) {
-  const newRequest = isCORSRequest(req.url, HOST_NAME) ? new Request(req.url, {mode: 'cors'}) :req;
-  return caches.match(newRequest).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
+// const handleFetchRequest = function(req) {
+//   const newRequest = isCORSRequest(req.url, HOST_NAME) ? new Request(req.url, {mode: 'cors'}) :req;
+//   return caches.match(newRequest).then(function(response) {
+//       // Cache hit - return response
+//       if (response) {
+//         return response;
+//       }
 
-      const fetchRequest = newRequest.clone();
-      return fetch(fetchRequest)
-        .then(function(response) {
-          // Check if we received an valid response
-          if (isValidResponse(response)) {
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME).then(function(cache) {
-                cache.put(newRequest, responseToCache);
-              });
-          }
+//       const fetchRequest = newRequest.clone();
+//       return fetch(fetchRequest)
+//         .then(function(response) {
+//           // Check if we received an valid response
+//           if (isValidResponse(response)) {
+//             const responseToCache = response.clone();
+//             caches.open(CACHE_NAME).then(function(cache) {
+//                 cache.put(newRequest, responseToCache);
+//               });
+//           }
 
-          return response;
-        });
-    });
+//           return response;
+//         });
+//     });
 
-};
+// };
 
-const onFetch = function(event) {
-	event.respondWith(handleFetchRequest(event.request));
-};
-// 更新事件
-self.addEventListener('fetch', onFetch);
+// const onFetch = function(event) {
+// 	event.respondWith(handleFetchRequest(event.request));
+// };
+// // 更新事件
+// self.addEventListener('fetch', onFetch);
